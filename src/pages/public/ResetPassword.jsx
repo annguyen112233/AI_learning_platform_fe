a7d554c82007f8db7e1605e2bb1bf7804db6760b
NguyenDanThanh <Thanhndse183476@fpt.edu.vn>
2026-01-27 19:39:25 +0700

    update page follow role

--- src/pages/public/ResetPassword.jsx
+++ src/pages/public/ResetPassword.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import { Lock, CheckCircle2, ArrowRight, ShieldCheck } from 'lucide-react';
 import Button from '@/components/ui/Button';
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,14 @@
+import { jwtDecode } from "jwt-decode";
+
 export const formatCurrency = (amount) => {
   return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(amount);
+};
+
+export const getUserRole = () => {
+  const token = sessionStorage.getItem("accessToken");
+
+if (token) {
+  const decoded = jwtDecode(token);
+  console.log(decoded);
+}
 };
